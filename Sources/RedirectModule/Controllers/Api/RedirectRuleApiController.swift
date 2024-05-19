fdba1ceaa01e594a7ca85d7984b797cf7cacf530
Oleg Ivanchenko <oleg@ivanchenko.ch>
2024-05-19 17:00:59 +0200

    Updated Redirect.Rule references to RedirectObjects.Redirect.Rule - Fixed "error: 'Redirect' is ambiguous for type lookup in this context extension Redirect.Rule.List: Content {}"
    The code has been modified to utilize RedirectObjects.Redirect.Rule instead of just Redirect.Rule. This update will ensure more explicit associativity and will help avoid conflicts or ambiguity in code referencing. The change extends across the ApiController and AdminController for the RedirectModule, and includes `listOutput`, `detailOutput`, `createInput`, `updateInput`, and `patchInput` functions.

--- Sources/RedirectModule/Controllers/Api/RedirectRuleApiController.swift
+++ Sources/RedirectModule/Controllers/Api/RedirectRuleApiController.swift
@@ -10,14 +10,14 @@ import Fluent
 import Feather
 import RedirectObjects
 
-extension Redirect.Rule.List: Content {}
-extension Redirect.Rule.Detail: Content {}
+extension RedirectObjects.Redirect.Rule.List: Content {}
+extension RedirectObjects.Redirect.Rule.Detail: Content {}
 
 struct RedirectRuleApiController: ApiController {
-    typealias ApiModel = Redirect.Rule
+    typealias ApiModel = RedirectObjects.Redirect.Rule
     typealias DatabaseModel = RedirectRuleModel
     
-    func listOutput(_ req: Request, _ models: [DatabaseModel]) async throws -> [Redirect.Rule.List] {
+    func listOutput(_ req: Request, _ models: [DatabaseModel]) async throws -> [RedirectObjects.Redirect.Rule.List] {
         models.map { model in
             .init(id: model.uuid,
                   source: model.source,
@@ -27,7 +27,7 @@ struct RedirectRuleApiController: ApiController {
         }
     }
     
-    func detailOutput(_ req: Request, _ model: DatabaseModel) async throws -> Redirect.Rule.List {
+    func detailOutput(_ req: Request, _ model: DatabaseModel) async throws -> RedirectObjects.Redirect.Rule.List {
         .init(id: model.id!,
               source: model.source,
               destination: model.destination,
@@ -35,21 +35,21 @@ struct RedirectRuleApiController: ApiController {
               notes: model.notes)
     }
     
-    func createInput(_ req: Request, _ model: DatabaseModel, _ input: Redirect.Rule.Create) async throws {
+    func createInput(_ req: Request, _ model: DatabaseModel, _ input: RedirectObjects.Redirect.Rule.Create) async throws {
         model.source = input.source
         model.destination = input.destination
         model.statusCode = input.statusCode
         model.notes = input.notes
     }
     
-    func updateInput(_ req: Request, _ model: DatabaseModel, _ input: Redirect.Rule.Update) async throws {
+    func updateInput(_ req: Request, _ model: DatabaseModel, _ input: RedirectObjects.Redirect.Rule.Update) async throws {
         model.source = input.source
         model.destination = input.destination
         model.statusCode = input.statusCode
         model.notes = input.notes
     }
     
-    func patchInput(_ req: Request, _ model: DatabaseModel, _ input: Redirect.Rule.Patch) async throws {
+    func patchInput(_ req: Request, _ model: DatabaseModel, _ input: RedirectObjects.Redirect.Rule.Patch) async throws {
         model.source = input.source
         model.destination = input.destination
         model.statusCode = input.statusCode
@@ -60,9 +60,9 @@ struct RedirectRuleApiController: ApiController {
         [
             KeyedContentValidator<String>.required("source", optional: optional),
             KeyedContentValidator<String>.required("destination", optional: optional),
-            KeyedContentValidator<Int>.contains("statusCode", Redirect.Rule.StatusCode.allCases.map(\.rawValue), optional: optional),
+            KeyedContentValidator<Int>.contains("statusCode", RedirectObjects.Redirect.Rule.StatusCode.allCases.map(\.rawValue), optional: optional),
 //            KeyedContentValidator<String>("source", "Source must be unique", optional: optional) { req, value in
-//                try await DatabaseModel.isUnique(req, \.$source == value, Redirect.Rule.getIdParameter(req))
+//                try await DatabaseModel.isUnique(req, \.$source == value, RedirectObjects.Redirect.Rule.getIdParameter(req))
 //            }
         ]
     }
